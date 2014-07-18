7b2a4764dc2a1c1dd7f466348756bca508730cb7
tomo watanabe <t.watanabe@freebit.net>
2014-07-18 15:13:51 +0900

    modify log name

--- app/src/main/java/com/tomovwgti/mqtt/push/ConnectionLog.java
+++ app/src/main/java/com/tomovwgti/mqtt/push/ConnectionLog.java
@@ -26,7 +26,7 @@ public class ConnectionLog {
     {
         if (!checkFlag()) return;
         File sdcard = Environment.getExternalStorageDirectory();
-        File logDir = new File(sdcard, "tokudu/log/");
+        File logDir = new File(sdcard, "mqtt/log/");
         if (!logDir.exists()) {
             logDir.mkdirs();
             // do not allow media scan
